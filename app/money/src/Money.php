818667306040bc9b1b6b3ebf199032e8a7e55d14
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-05-06 17:28:58 +0900

    第8章 実装を隠す

--- app/money/src/Money.php
+++ app/money/src/Money.php
@@ -2,8 +2,10 @@
 
 namespace Money;
 
-class Money
+abstract class Money
 {
+    abstract public function times(int $multiplier): Money;
+
     public function __construct(protected readonly int $amount)
     {
     }
@@ -12,5 +14,15 @@ class Money
     {
         return $this->amount == $money->amount
             && $this::class === $money::class;
+    }
+
+    public static function dollar(int $amount): Money
+    {
+        return new Dollar($amount);
+    }
+
+    public static function franc(int $amount): Money
+    {
+        return new Franc($amount);
     }
 }
