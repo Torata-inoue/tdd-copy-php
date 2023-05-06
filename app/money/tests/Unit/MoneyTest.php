5a10195812d78ad42c6650a8ad7fec90e72471f3
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-05-06 14:28:17 +0900

    仮実装

---
+++ app/money/tests/Unit/MoneyTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Money\Tests\Unit;
+
+use Money\Dollar;
+use PHPUnit\Framework\TestCase;
+
+class MoneyTest extends TestCase
+{
+    public function testMultiplication()
+    {
+        $five = new Dollar(5);
+        $five->times(2);
+
+        $this->assertEquals(10, $five->amount);
+    }
+
+}
